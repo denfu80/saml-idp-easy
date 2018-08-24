df869466a7c40955e4cbfe3eab555bfdf6f3c707
Dennis Fuhrmann <dennis.fuhrmann@signavio.com>
2018-08-24 11:04:00 +0200

    rudiment server start and documentation

---
+++ src/main/java/idpeasy/app/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Module responsible to start application.
+ * <p>
+ * Use {@link idpeasy.app.Application#main(java.lang.String[])} method to start.
+ * <br/>
+ * <br/>
+ * <p>
+ * Default <b>port</b> is configured in application.properties file in resources directory
+ */
+package idpeasy.app;
